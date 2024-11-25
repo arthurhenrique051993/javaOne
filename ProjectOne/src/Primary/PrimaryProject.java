a9aa2ae6f3011539ac2af064d7c0e7e9345e5fc9
ArThUrHeNrIqUeNuNeSbArBoSa <arthurhenrique051993@gmail.com>
2024-11-25 09:33:09 -0300

    subir uma nova alteração

--- ProjectOne/src/Primary/PrimaryProject.java
+++ ProjectOne/src/Primary/PrimaryProject.java
@@ -3,6 +3,7 @@ package Primary;
 public class PrimaryProject {
 	public static void main (String []args) {
 		System.out.println("teste para sistema");
+		System.out.println("alterção para sistema");
 	}
 
 }
