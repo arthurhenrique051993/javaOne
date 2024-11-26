4f099128c8ab6ae3851ae067284368a3f03df239
arthurhenrique051993 <arthurhenrique051993@gmail.com>
2024-11-26 09:21:37 -0300

    iniciando um quiz

--- ProjectOne/src/Primary/PrimaryProject.java
+++ ProjectOne/src/Primary/PrimaryProject.java
@@ -3,9 +3,8 @@ package Primary;
 public class PrimaryProject {
 	public static void main (String []args) {
 		System.out.println("teste para sistema");
-		System.out.println("alterção para sistema");
-		System.out.println("nova alteração");//roda diacho 
-		System.out.println("nova alteração");//roda diacho 
+		System.out.println("iniciando um quiz");//quiz
+		
 		
 	}
 
